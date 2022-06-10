4b653d85a6cb2e01d6cd63ff038384a6fc1478c2
Dmitry <gldimon@mail.ru>
2022-06-10 15:22:42 +0700

    Избавился от избыточных проверок булевых значений.

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -2,9 +2,9 @@ import {FilterType} from '../const.js';
 
 const filter = {
   [FilterType.ALL]: (films) => films,
-  [FilterType.WATCHLIST]: (films) => films.filter((item) => item.userDetails.watchlist === true),
-  [FilterType.WATCHED]: (films) => films.filter((item) => item.userDetails.alreadyWatched === true),
-  [FilterType.FAVORITES]: (films) => films.filter((item) => item.userDetails.favorite === true),
+  [FilterType.WATCHLIST]: (films) => films.filter((item) => item.userDetails.watchlist),
+  [FilterType.WATCHED]: (films) => films.filter((item) => item.userDetails.alreadyWatched),
+  [FilterType.FAVORITES]: (films) => films.filter((item) => item.userDetails.favorite),
 };
 
 export {filter};
